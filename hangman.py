21cdaa57c0b0fb0c5d22a3d22908a897e81d0b7e
Malik <97180802+mjones97@users.noreply.github.com>
2023-05-23 23:01:23 -0400

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,62 +1,69 @@
 import random
-import string
 
 def hangman():
-    # Prompt user to create a username
-    username = input("Enter username: ")
-    # Set up target words
-    words = ['python', 'programming', 'technology', 'hangman', 'game']
-    # Randomly choose one of the target words
-    word = random.choice(words).lower()
-    # Created an empty list to keep track of guessed letters
+    #Prompt a username from the user
+    username = input("Enter a username: ")
+    # List of words to choose from
+    words = ['apple', 'banana', 'orange', 'grape', 'watermelon']
+    # Select a random word from the list
+    word = random.choice(words)
+    # List to store guessed letters
     guessed_letters = []
-    # Number of total attempts
+    # Number of attempts allowed
     attempts = 6
-   
-    print("Welcome to Hangman" + ' ' + username + '!')
-   
-    while True:
-        # Display current word with blanks for unguessed letters
-        display_word = ""
+    # Print welcome message
+    print("Welcome to my Python Hangman Game" + " " + username + "!")
+    # Print initial representation of the word with underscores
+    print("_ " * len(word))
+
+    # Main game loop
+    while attempts > 0:
+        # Prompt the player to enter a letter guess
+        guess = input("Enter a letter: ").lower()
+
+        # Check if the letter has already been guessed
+        if guess in guessed_letters:
+            print("You've already guessed that letter. Try again.")
+            continue
+
+        # Add the guessed letter to the list of guessed letters
+        guessed_letters.append(guess)
+
+        # Check if the guessed letter is not in the word
+        if guess not in word:
+            # Decrement the number of attempts
+            attempts -= 1
+            print("Wrong guess. You have", attempts, "attempts left.")
+
+        # Construct the revealed word with guessed letters and underscores
+        revealed_word = ""
         for letter in word:
             if letter in guessed_letters:
-                display_word += letter
+                revealed_word += letter + " "
             else:
-                display_word += "_"
-        print("\n" + display_word)
+                revealed_word += "_ "
 
-        # Print congrats message when word is guessed correctly
-        if display_word == word:
-            print("\nCongratulations! You guessed the word correctly.")
-            break
+        # Print the revealed word
+        print(revealed_word)
 
-        # Print game over message with solutuon after max numbers of attempts
-        if attempts == 0:
-            print("\nGame over! You ran out of attempts. The word was:", word)
-            break
-        
-        # Prompt the user to enter a letter
-        guess = input("\nEnter a letter: ").lower()
-       
-        # Error message when more than one letter is entered at once
-        if len(guess) != 1 or not guess.isalpha():
-            print("Invalid input. Please enter a single letter.")
-            continue
-       
-        # Error message when guessing a letter that has been used already
-        if guess in guessed_letters:
-            print("You already guessed that letter. Try again.")
-            continue
-       
-        # Add guessed letters to list
-        guessed_letters.append(guess)
-       
-        # display correct/incorrect message when letter is guessed
-        if guess in word:
-            print("Correct guess!")
-        else:
-            # when guessed incorrectly, the user loses an attempt
-            attempts -= 1
-            print("Wrong guess. Attempts remaining:", attempts)
+        # Check if the word has been completely guessed
+        if "_" not in revealed_word:
+            # Prompt the player to play again
+            play_again = input("Do you want to play again? (yes/no): ")
+            if play_again.lower() == "yes":
+                hangman()  # Restart the game by calling the hangman() function recursively
+            else:
+                print("Thank you for playing Hangman!")
+                return
 
+    # Game over, player ran out of attempts
+    print("Game over! You've run out of attempts.")
+    # Prompt the player to play again
+    play_again = input("Do you want to play again? (yes/no): ")
+    if play_again.lower() == "yes":
+        hangman()  # Restart the game by calling the hangman() function recursively
+    else:
+        print("Thank you for playing Hangman!")
+
+# Call the hangman function to start the game
 hangman()
